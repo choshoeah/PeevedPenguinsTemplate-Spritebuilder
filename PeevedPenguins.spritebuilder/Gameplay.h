1ad9f94726104d063a1d1ff50c89195ca32b3280
choshoeah <josh.chung@hotmail.com>
2014-12-18 03:10:06 +1100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Joshua Chung on 18/12/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
