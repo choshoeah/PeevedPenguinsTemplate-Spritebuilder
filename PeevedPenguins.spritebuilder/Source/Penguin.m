c13f58dbd516e639ebafe047e0b267a6d390960c
choshoeah <josh.chung@hotmail.com>
2014-12-18 02:58:09 +1100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Penguin.m
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -10,14 +10,5 @@
 
 @implementation Penguin
 
-- (id)init {
-    self = [super init];
-    
-    if (self) {
-        CCLOG(@"Seal created");
-    }
-    
-    return self;
-}
 
 @end
--- PeevedPenguins.spritebuilder/Source/Seal.m
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -10,14 +10,5 @@
 
 @implementation Seal
 
-- (id)init {
-    self = [super init];
-    
-    if (self) {
-        CCLOG(@"Penguin created");
-    }
-    
-    return self;
-}
 
 @end
